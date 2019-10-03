49527a4b5814fbf81e24acd1e0fa1010e448e898
Arniox <nikkdiehl@gmail.com>
2019-10-03 21:51:44 +1300

    Temporarly removed everything to shift around files

--- COMP710-2019-S2/students/nikkolas.diehl/Unit 1 - Exercises/Simple Dice Game - Ex1/HeaderEx1.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-//Define all functions
-void GeneratePlayers();
-void CheckCheating(int computerIsCheatingValue);
-void PlayRound(int roundCount);
-void checkWhoWon();
-void input();
